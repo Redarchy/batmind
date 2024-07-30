1e6524c85c4664d40d23396c03c57c784a58eb31
anilb\Anil Basibuyuk <anilbbyk98@gmail.com>
2024-07-31 00:05:29 +0300

    Add test condition

---
+++ Batmind/Runtime/Tree/Nodes/Conditions/IsDayOfMonthPassedCondition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Batmind.Tree.Nodes.Conditions
+{
+    public class IsDayOfMonthPassedCondition : ConditionNode
+    {
+        [SerializeField] private int _DayToPass;
+
+        protected override bool IsConditionSatisfied()
+        {
+            var today = DateTime.Now.Day;
+
+            return today > _DayToPass;
+        }
+    }
+}
