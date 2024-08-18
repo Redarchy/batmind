a4208a79026930d634fefd79935d6b5d27c06843
anilb\Anil Basibuyuk <anilbbyk98@gmail.com>
2024-08-19 00:52:29 +0300

    Add reset method for changing values based on runtime for reusable objects

--- Batmind/Runtime/Tree/BehaviourContext.cs
+++ Batmind/Runtime/Tree/BehaviourContext.cs
@@ -3,6 +3,9 @@
     [System.Serializable]
     public class BehaviourContext
     {
-        
+        protected virtual void Reset()
+        {
+            // Implement for the reusable objects to remove references or set default values that change in runtime.
+        }
     }
 }
