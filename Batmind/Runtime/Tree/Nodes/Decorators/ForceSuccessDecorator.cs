73282aa2039ac20b4a4d8b08335e177dfef6db2e
anilb\Anil Basibuyuk <anilbbyk98@gmail.com>
2024-09-14 20:41:32 +0300

    Add new decorators

---
+++ Batmind/Runtime/Tree/Nodes/Decorators/ForceSuccessDecorator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Batmind.Tree.Nodes.Decorators
+{
+    [Serializable]
+    public class ForceSuccessDecorator : DecoratorNode
+    {
+        public override Status Process()
+        {
+            return Status.Success;
+        }
+    }
+}
---
+++ Batmind/Runtime/Tree/Nodes/Decorators/RunningDecorator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Batmind.Tree.Nodes.Decorators
+{
+    [System.Serializable]
+    public class RunningDecorator : DecoratorNode
+    {
+        [SerializeField] private Status _DecoratedValue = Status.Running;
+
+        public override Status Process()
+        {
+            switch (Child.Process())
+            {
+                case Status.Running:
+                    return _DecoratedValue;
+                case Status.Failure:
+                    return Status.Failure;
+                case Status.Success:
+                    return Status.Success;
+                default:
+                    return Status.Success;
+            }
+        }
+    }
+}
